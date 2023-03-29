a7b4ef609d3c82b0a52db3b7774dc63eb46c3fd7
Philippe Brunet <phil_27mo@hotmail.com>
2023-03-28 21:26:30 -0400

    generate myLibrary

---
+++ projects/my-library/src/lib/my-library.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-myLibrary',
+  template: `
+    <p>
+      my-library works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MyLibraryComponent {
+
+}
